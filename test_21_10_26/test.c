685d7ccad6af0e70fc658545ea774eeff9be73e8
TauCeti0207 <2076188013@qq.com>
2021-10-26 22:58:54 +0800

    21_10_26 数据结构02 leetcode

--- test_21_10_26/test.c
+++ test_21_10_26/test.c
@@ -91,15 +91,87 @@
 //	printf("最大公约数为:%d\n", b);
 //	return 0;
 //}
-#include<time.h>
-long long Fibonacci(size_t N)
+//#include<time.h>
+//long long Fibonacci(size_t N)
+//{
+//	return N < 2 ? N : Fibonacci(N - 1) + Fibonacci(N - 2);
+//}
+//int main()
+//{
+//	int begin = clock();
+//	Fibonacci(45);
+//	int end = clock();
+//	printf("%d\n", end - begin);
+//}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ //returnSize 表示找出来的2个数得返回,以数组的形式返回
+
+//int* singleNumbers(int* nums, int numsSize, int* returnSize)
+//{
+//    int ret = 0;
+//    for (int i = 0; i < numsSize; i++)
+//    {
+//        ret ^= nums[i];
+//    }
+//    //从低到高去找ret里一个为1的位
+//    int j = 0;
+//    for (; j < 32; ++j)
+//    {
+//        if (ret & (1 << j)) // if(ret>>j & 1)
+//        {
+//            break;
+//        }
+//        // ret的第j位为1,说明x y两个的第j位一个为1,一个为0
+//    }
+//    // 将原数组分为2组,第j位为1的一组,第j位为0的一组
+//    //x y一定会分别进入a b组,其他出现2次的数要么进a组,要么进b组
+//    //a组和b组数据就会变成其他数出现2次,只有一个数出现1次,再分别异或就行
+//    int x = 0;
+//    int y = 0;
+//    for (int k = 0; k < numsSize; ++k)
+//    {
+//        if (nums[k] & (1 << j))    //a组
+//        {
+//            x ^= nums[k];
+//        }
+//        else                    //b组
+//        {
+//            y ^= nums[k];
+//        }
+//    }
+//    int* arr = (int*)malloc(sizeof(int) * 2);
+//    arr[0] = x;
+//    arr[1] = y;
+//    *returnSize = 2;//输出型参数
+//    return arr;
+//}
+
+int singleNumber(int* nums, int numsSize)
 {
-	return N < 2 ? N : Fibonacci(N - 1) + Fibonacci(N - 2);
-}
-int main()
-{
-	int begin = clock();
-	Fibonacci(45);
-	int end = clock();
-	printf("%d\n", end - begin);
+    int arr[32] = { 0 };
+    int count = 0;
+    int k = 31;
+    for (int i = 0; i < 32; i++)
+    {
+        for (int j = 0; j < numsSize; j++)
+        {
+            count += ((nums[j] >> i) & 1);
+        }
+        arr[k] = count;
+        count = 0;
+        k--;
+    }
+    for (int i = 0; i < 32; i++)
+    {
+        arr[i] %= 3;
+    }
+    int ret = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        ret |= (arr[31] << i);
+    }
+    return ret;
 }
