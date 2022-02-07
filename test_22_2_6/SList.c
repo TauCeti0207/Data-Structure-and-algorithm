49a0505b11c1299d916b87fd976d64f96eafa62a
TauCeti0207 <81548397+TauCeti0207@users.noreply.github.com>
2022-02-07 23:36:27 +0800

    Add files via upload

--- test_22_2_6/SList.c
+++ test_22_2_6/SList.c
@@ -73,3 +73,98 @@ void SListPopBack(SLTNode** pplist) {
 		prev->next = NULL;//尾删时要将最后一个结点的上一个结点的next置为NULL才行
 	}
 }
+
+void SListPopFront(SLTNode** pplist) {
+	//1.没有节点
+	//2.一个节点
+	//3.多个节点
+	if (*pplist == NULL) {
+		return;
+	}
+	else {
+		//恰好适用一个节点和多个节点的情况
+		//保存plist->next,如果直接free plist就找不到后面的空间了
+		SLTNode* next = (*pplist)->next;
+		free(*pplist);
+		*pplist = next;//因为要修改plist必须传址调用
+	}
+}
+
+
+//单链表查找
+SLTNode* SListFind(SLTNode* plist, SLTDataType x) {
+	SLTNode* cur = plist;
+	//while(cur != NULL)
+	while (cur) {
+		if (cur->data == x) {
+			return cur;
+			//查找兼具修改的作用
+		}
+		cur = cur->next;
+	}
+	return NULL;
+}
+
+
+//在pos后面插入
+void SListInserAfter(SLTNode* pos, SLTDataType x) {
+	assert(pos);
+	SLTNode* newnode = BuySLTNode(x);
+	//注意顺序不要反了
+	newnode->next = pos->next;
+	pos->next = newnode;
+}
+
+//反了需要将pos->nxet提前保存起来
+//在pos后面插入
+/* void SListInserAfter(SLTNode* pos, SLTDataType x) {
+	assert(pos);
+	SLTNode* newnode = BuySLTNode(x);
+	SLTNode* next = pos->next;
+	pos->next = newnode;
+	newnode->next = next;
+}
+*/
+
+
+
+//在pos之前插入
+void SListInserBefore(SLTNode** pplist, SLTNode* pos, SLTDataType x) {
+	assert(pos);
+	SLTNode* newnode = BuySLTNode(x);
+	//如果pos在第一个呢?
+	if (pos == *pplist) {//相当于头插
+		newnode->next = pos;
+		*pplist = newnode;
+	}
+	else {
+		SLTNode* prev = NULL;
+		SLTNode* cur = *pplist;
+		while (cur != pos) {
+			prev = cur;
+			cur = cur->next;
+		}
+		//避免prev为空
+		if (prev == NULL) {
+			return;
+		}
+		prev->next = newnode;
+		newnode->next = pos;
+	}
+}
+
+
+//在pos之后擦除
+void SListEraseAfter(SLTNode* pos) {
+	assert(pos);
+	//pos之后无节点
+	if (pos->next == NULL) {
+		return;
+	}
+	else {
+		SLTNode* next = pos->next;
+		pos->next = next->next;
+		free(next);
+		next = NULL;
+	}
+}
